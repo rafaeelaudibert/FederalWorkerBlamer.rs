1563ee177eff1d537ab694e6115e8979ce98de9f
RafaAudibert <rafaeelaudibert@gmail.com>
2018-11-24 04:25:10 -0200

    :bug: Fix bug when trying to read the observation line

--- lib/script.py
+++ lib/script.py
@@ -2,7 +2,7 @@ import csv
 
 with open("csv/201809_Remuneracao.csv", "r") as f:
     dados = csv.reader(f, delimiter=";")
-    lista = list(dados)[1:]
+    lista = list(dados)[1:-1]
     ordenada = sorted(lista, key= lambda val : (val[4], int(val[2])))
 
     with open("csv/201809_RemuneracaoParsed.csv", "w") as write_file:
