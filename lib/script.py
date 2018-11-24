1b6af7d08857f3529fde770667aa8f926f154dc9
RafaAudibert <rafaeelaudibert@gmail.com>
2018-11-23 23:05:52 -0200

    :bug: Fix bugs with paths in python

--- lib/script.py
+++ lib/script.py
@@ -1,11 +1,11 @@
 import csv
 
-with open("csv/201808_Remuneracao.csv", "r") as f:
+with open("../csv/201808_Remuneracao.csv", "r") as f:
     dados = csv.reader(f, delimiter=",")
     lista = list(dados)[1:]
     ordenada = sorted(lista, key= lambda val : (val[4], int(val[2])))
 
-    with open("csv/201808_RemuneracaoParsed.csv", "w") as write_file:
+    with open("../csv/201808_RemuneracaoParsed.csv", "w") as write_file:
         writer = csv.writer(write_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
         for element in ordenada:
             writer.writerow(element)
